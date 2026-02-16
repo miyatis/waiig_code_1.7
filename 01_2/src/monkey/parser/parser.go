d03a211e5e66b57a96e14e0cff8e83cc921632b1
n7n788 <yuki1113.2001@gmail.com>
2026-02-17 00:30:49 +0900

    add: Parserを実装

---
+++ 01_2/src/monkey/parser/parser.go
@@ -0,0 +1,28 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// 2つのトークンを読み込む。curToken と peekToken をセットするため。
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() *ast.Program {
+	return nil
+}
