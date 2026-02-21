92e54ff49c8843e562180998572f61792bd367f2
n7n788 <yuki1113.2001@gmail.com>
2026-02-22 00:11:42 +0900

    add: 予期しないtokenがきたらErrorを出力するようにした

--- work/src/monkey/parser/parser.go
+++ work/src/monkey/parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
+	"fmt"
 )
 
 type Parser struct {
@@ -11,10 +12,15 @@ type Parser struct {
 
 	curToken token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 
 	// 2つのトークンを読み込む。curToken と peekToken をセットするため。
 	p.nextToken()
@@ -86,7 +92,18 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+			t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
